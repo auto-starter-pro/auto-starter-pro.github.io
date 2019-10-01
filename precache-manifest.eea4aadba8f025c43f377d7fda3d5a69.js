a51ca23bb0c3931e397e50183cb5fad6c305e5d1
Rafael Hovhannisyan <hovhannisian.rafael@gmail.com>
2019-10-02 00:51:11 +0400

    Puff commit

---
+++ precache-manifest.eea4aadba8f025c43f377d7fda3d5a69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68a30e54c6102ad47be7819cdc60689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df55be7e8bdaa25e9721",
+    "url": "/static/css/main.fa2d7244.chunk.css"
+  },
+  {
+    "revision": "c48d857ac171d21c7632",
+    "url": "/static/js/2.32d2a5a1.chunk.js"
+  },
+  {
+    "revision": "df55be7e8bdaa25e9721",
+    "url": "/static/js/main.91026b54.chunk.js"
+  },
+  {
+    "revision": "94c949cd8765cb9b2a81",
+    "url": "/static/js/runtime~main.04300612.js"
+  }
+]);
